3c4e50b0b3518b80847cb1d6408a7aabf7140ca9
naveenkumar1710 <113573484+naveenkumar1710@users.noreply.github.com>
2022-09-27 12:58:28 +0530

    Add files via upload

---
+++ vikram.java
@@ -0,0 +1,9 @@
+public class vikram{
+static String newComputer(String brand){
+//int price=500000;
+return brand;
+}
+public static void main(String[]args){
+System.out.println(vikram.newComputer("Dell"));
+}
+}
